c657187c6b23eb9ccbf734cb623832af9d2a3149
Natalia Popova <sverdlova3@gmail.com>
2022-12-13 11:30:53 +0500

    add __str__

--- book_app/models.py
+++ book_app/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=40)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.title} - {self.rating}%'
