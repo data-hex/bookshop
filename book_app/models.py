14322ca88715a68115a8f3cfbd9ca744b56c18d0
Natalia Popova <sverdlova3@gmail.com>
2022-12-14 11:30:52 +0500

    new fields in Book

--- book_app/models.py
+++ book_app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=40)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f'{self.title} - {self.rating}%'
